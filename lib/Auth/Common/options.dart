501a4c551fa129f7fd420d68f83bf06a04de3e6c
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2023-11-16 12:02:43 +0500

    options separate file

---
+++ lib/Auth/Common/options.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const firebaseOptions = FirebaseOptions(
+       apiKey: "AIzaSyB8c9oDWVNND0mM2UCy1nvK-QP11ltMskE",
+  authDomain: "mad-poject.firebaseapp.com",
+  projectId: "mad-poject",
+  storageBucket: "mad-poject.appspot.com",
+  messagingSenderId: "730271059087",
+  appId: "1:730271059087:web:3532efc7a6205cea7df4fa"
+        );
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:project/Auth/Common/options.dart';
 import 'package:project/MainPage/main_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-       apiKey: "AIzaSyB8c9oDWVNND0mM2UCy1nvK-QP11ltMskE",
-  authDomain: "mad-poject.firebaseapp.com",
-  projectId: "mad-poject",
-  storageBucket: "mad-poject.appspot.com",
-  messagingSenderId: "730271059087",
-  appId: "1:730271059087:web:3532efc7a6205cea7df4fa"
-        )
+    options:  firebaseOptions
   ); 
   
   runApp(const MainApp());
